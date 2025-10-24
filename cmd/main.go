42ffe25517191e8363f994dec7f19dcdb7def9f1
Anh Ha <anhvanhoa.it@gmail.com>
2025-10-24 16:13:11 +0700

    Add Docker support and configuration updates for CostService
    - Introduced .dockerignore to exclude unnecessary files from Docker builds.
    - Updated .gitignore to include token files for security.
    - Refactored docker-compose.yml to define a new cost-service with health checks and secrets management.
    - Created DockerFile for building the CostService application with multi-stage builds.
    - Adjusted environment configuration in bootstrap/env.go to use cost_service.config in production.
    - Cleaned up main.go by removing unused discovery code and ensuring proper gRPC client initialization.
    - Enhanced Makefile with a new target for building and running the application in Docker.

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 	"cost_service/infrastructure/grpc_service"
 	cost_tracking_service "cost_service/infrastructure/grpc_service/cost_tracking"
 
-	"github.com/anhvanhoa/service-core/domain/discovery"
 	gc "github.com/anhvanhoa/service-core/domain/grpc_client"
 )
 
@@ -19,20 +18,6 @@ func StartGRPCServer() {
 	app := bootstrap.App()
 	env := app.Env
 	log := app.Log
-
-	discoveryConfig := &discovery.DiscoveryConfig{
-		ServiceName:   env.NameService,
-		ServicePort:   env.PortGrpc,
-		ServiceHost:   env.HostGprc,
-		IntervalCheck: env.IntervalCheck,
-		TimeoutCheck:  env.TimeoutCheck,
-	}
-
-	discovery, err := discovery.NewDiscovery(discoveryConfig)
-	if err != nil {
-		log.Fatal("Failed to create discovery: " + err.Error())
-	}
-	discovery.Register()
 
 	clientFactory := gc.NewClientFactory(env.GrpcClients...)
 	permissionClient := grpc_client.NewPermissionClient(clientFactory.GetClient(env.PermissionServiceAddr))
